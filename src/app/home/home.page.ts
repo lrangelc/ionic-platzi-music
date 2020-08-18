8b2965748e5648ebcaab54dd5f35747685727d07
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-17 21:04:08 -0600

    add intro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  slideOpt = { initialSlid: 0, slidesPerView: 1, centeredSlides: true, speed: 400 };
-
-  slides = [
-    { title: 'Este es el tituloe', subTitle: 'Este es el subtitulo', description: '', icon: 'play' },
-    { title: '', subTitle: '', description: '', icon: '' },
-    { title: '', subTitle: '', description: '', icon: '' },
-    { title: '', subTitle: '', description: '', icon: '' },
-  ];
   constructor() {}
 }

c778a4d3eceed377ac5bdef4971473401dd0e829
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-19 11:24:36 -0600

    add validations to login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  validationMessage = {
+    email: [
+      { type: 'required', message: 'El email es requerido' },
+      {
+        type: 'email',
+        message: 'ojo! este no es un email valido',
+      },
+    ],
+    password: [
+      { type: 'required', message: 'El password es requerido' },
+      {
+        type: 'minlength',
+        message: 'Tamaño minimo 6 caracteres',
+      },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+    });
   }
 
+  ngOnInit() {}
+
+  loginUser() {}
 }

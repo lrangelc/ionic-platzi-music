040c177b9b3703f71f3eeed3742c0cf6fc785b43
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-28 11:11:07 -0600

    add settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,10 @@ const routes: Routes = [
     path: 'songs-modal',
     loadChildren: () => import('./songs-modal/songs-modal.module').then( m => m.SongsModalPageModule)
   },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
 ];
 
 @NgModule({

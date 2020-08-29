6b1d94a69813afcea2bf8dc5f03d3e481cb4b51d
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-29 13:48:38 -0600

    add sports page maps

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -12,10 +12,13 @@ const routes: Routes = [
         path: 'home',
         loadChildren: () => import('./../home/home.module').then((m) => m.HomePageModule),
       },
-
       {
         path: 'settings',
         loadChildren: () => import('./../settings/settings.module').then((m) => m.SettingsPageModule),
+      },
+      {
+        path: 'sports',
+        loadChildren: () => import('./../sports/sports.module').then((m) => m.SportsPageModule),
       },
       {
         path: '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapsKeyApi: 'AIzaSyBW6I-Aj52wbtYD2mwB6rYWCsC_VbCtS8Y',
 };
 
 /*
